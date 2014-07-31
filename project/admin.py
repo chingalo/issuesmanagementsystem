245cb4212ca950edc785b277e58c27d67f811beb
chingalo <profschingalo@gmail.com>
2014-07-31 16:44:25 +0300

    add admin file

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from project.models import *
 
-# Register your models here.
+
+
+admin.site.register(Project_details)
+admin.site.register(Users)
+admin.site.register(Project_assignment)
+admin.site.register(Issue)
+admin.site.register(Comments)
+admin.site.register(Issue_assignment)
+admin.site.register(Issue_status)
