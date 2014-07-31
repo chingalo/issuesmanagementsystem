616f54e9071c3758c1bdf45519ec941c13725b5d
chingalo <profschingalo@gmail.com>
2014-07-31 12:22:41 +0300

    remove sql db and add urls stting for project

--- reportissues/urls.py
+++ reportissues/urls.py
@@ -1,12 +1,23 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'reportissues.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    
     url(r'', include('project.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+    
+    
+)+ staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+		url(r'^', include('project.urls')),
+	url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT,
+        'show_indexes': True}),
+        url(r'',
+        include('django.contrib.staticfiles.urls')),
+    ) + urlpatterns
