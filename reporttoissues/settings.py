59c9911dac4ba1b3def011e6ae29e6a36b575537
chingalo <profschingalo@gmail.com>
2014-08-04 00:05:15 +0300

    setting

--- reporttoissues/settings.py
+++ reporttoissues/settings.py
@@ -1,30 +1,111 @@
+"""
+Django settings for reportissues project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-gettext = lambda s: s
-PROJECT_PATH = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'o(%)7bqv6s_-wrwr+zaqqhb_h4u4145x71(w54ua45d()x)3on'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-ADMINS = (
-     ('joseph chingalo', 'profschingalo@gmail.com'),
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'project',
 )
-MANAGERS = ADMINS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'reportissues.urls'
+
+WSGI_APPLICATION = 'reportissues.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dcsvqcro55ts0a',
+        'HOST': 'ec2-54-204-43-200.compute-1.amazonaws.com',
+        'PASSWORD':'REZQm4v374ulgH2yRh3ahKwcmd',
+        'PORT':'5432',
+        'USER':'aeglmpmexabvnl',
+         
     }
 }
-ALLOWED_HOSTS = ['*']
-TIME_ZONE = 'Africa/Dar_es_Salaam'
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
+
+TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
-MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
-MEDIA_URL = "/media/"
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+gettext = lambda s: s
+PROJECT_PATH = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
@@ -33,23 +114,23 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-SECRET_KEY = '8#pdho3q*cvh4b#u8l_v7$mvvg+6jx#=r9=qhs0ksyp+en0&bt'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
     'django.template.loaders.eggs.Loader',
     )
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.doc.XViewMiddleware',   
+TEMPLATE_DIRS = (
+       os.path.join(PROJECT_PATH, "templates"),
+)    
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    
 )
-
+ 
 #E-mail sending configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_IMAP_HOST = 'imap.gmail.com'
@@ -59,99 +140,3 @@ EMAIL_HOST_PASSWORD = 'projectmanagement'
 EMAIL_USE_TLS = True
 EMAIL_SENDER = 'projecmanagementspprt@gmail.com'
 EMAIL_SSL = True
-    
-ROOT_URLCONF = 'reporttoissues.urls'
-WSGI_APPLICATION = 'reporttoissues.wsgi.application'
-TEMPLATE_DIRS = (
-       os.path.join(PROJECT_PATH, "templates"),
-)
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.admin',
-    'django.contrib.admindocs',
-    'django.contrib.staticfiles',     
-    'project',  
-)
-LANGUAGES = [
-    ('en', 'English'),
-]
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.request',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    
-)  
-CMS_TEMPLATES = ( 
-	 
-	#('home.html', gettext('pages')),	              
-   )
-   
-   
-CMS_LANGUAGES = {
-    1: [
-        {
-            'code': 'en',
-            'name': gettext('English'),
-            'fallbacks': ['de', 'fr'],
-            'public': True,
-            'hide_untranslated': True,
-            'redirect_on_fallback':False,
-        },
-        {
-            'code': 'de',
-            'name': gettext('Deutsch'),
-            'fallbacks': ['en', 'fr'],
-            'public': True,
-        },
-        {
-            'code': 'fr',
-            'name': gettext('French'),
-            'public': False,
-        },
-    ],
-    2: [
-        {
-            'code': 'nl',
-            'name': gettext('Dutch'),
-            'public': True,
-            'fallbacks': ['en'],
-        },
-    ],
-    'default': {
-        'fallbacks': ['en', 'de', 'fr'],
-        'redirect_on_fallback':True,
-        'public': False,
-        'hide_untranslated': False,
-    }
-}  
-CMS_SOFTROOT = True
